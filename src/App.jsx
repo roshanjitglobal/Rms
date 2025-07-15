57bb42c8dfd5df257c7de958f96f0e2c6e398ba8
priyadharshinip24 <priyadarshini.p@jitgolbalinfosystems.com>
2025-07-15 16:55:11 +0530

    initlal push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="text-4xl text-blue-600 font-bold text-center mt-10">
+      Tailwind CSS is working!
+    </div>
+  );
+};
+
+export default App;
+
