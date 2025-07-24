c833e101bf8b612bb6f614f0e7b58d185409d7cc
roshanjitglobal <roshan.s@jitglobalinfosystems.com>
2025-07-24 13:29:08 +0530

    wwwef

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, 
-    port: 5173,
-  }
-})
+// vite.config.js
+export default {
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'vendor-react'; // Create separate chunk for React
+            }
+            return 'vendor'; // Other node_modules go here
+          }
+        },
+      },
+    },
+  },
+};
+
