10001f445cd7323110a9755389492a773129d56f
roshanjitglobal <roshan.s@jitglobalinfosystems.com>
2025-07-29 12:38:37 +0530

    inital pushed for testing purpose

--- src/pages/Candidate/Application.jsx
+++ src/pages/Candidate/Application.jsx
@@ -212,7 +212,7 @@ function MyApplications() {
                     </div>
                   </div>
 
-                  <div className="mt-4 flex justify-end">
+                  {/* <div className="mt-4 flex justify-end">
                     <button
                       type="button"
                       className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
@@ -220,7 +220,7 @@ function MyApplications() {
                       <FiExternalLink className="-ml-1 mr-2 h-4 w-4" />
                       View Details
                     </button>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             ))
